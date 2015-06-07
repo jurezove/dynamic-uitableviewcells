81df01ebee15cc9addd6cd366e9319dc2ba3eca1
Jure Žove <jure.pase.kure@gmail.com>
2015-06-07 19:58:39 +0200

    dynamic row heights with images fetched from a dropbox folder

--- Dynamic Table View Cells/DynamicHeightTableViewController.m
+++ Dynamic Table View Cells/DynamicHeightTableViewController.m
@@ -8,18 +8,18 @@
 
 #import "DynamicHeightTableViewController.h"
 #import "DynamicTableViewCell.h"
-#import "FlickrData.h"
-#import "PhotoFetcher.h"
 #import "AFNetworking.h"
 #import "UIImageView+AFNetworking.h"
 
 @interface DynamicHeightTableViewController ()
 
-@property (nonatomic, strong) FlickrData *flickrData;
 @property (nonatomic) NSInteger page;
 @property (nonatomic, strong) NSMutableArray *tableRowHeights;
+@property (nonatomic, strong) NSMutableArray *imageURLs;
 
 @end
+
+static NSString *kStaticDropBoxURL = @"https://dl.dropboxusercontent.com/u/25733120/Zonda/";
 
 @implementation DynamicHeightTableViewController
 
@@ -37,22 +37,16 @@
 
 - (void)reload {
     self.page = 1;
-    __weak typeof(self) weakSelf = self;
-    [[PhotoFetcher sharedFetcher] fetchFlickrImagesWithTags:@[@"bali"]
-                                                    andPage:self.page
-                                                    success:^(FlickrData *newData) {
-                                                        if (weakSelf.page == 1)
-                                                            weakSelf.flickrData = newData;
-                                                        else
-                                                            [weakSelf.flickrData appendPhotos:newData.photos];
-                                                        
-                                                        // Set default row heights to tableView.rowHeight
-                                                        [weakSelf setDefaultRowHeights];
-                                                        
-                                                        [weakSelf.tableView reloadData];
-                                                    } error:^(NSURLSessionDataTask *task, NSError *error) {
-                                                        NSLog(@"Error: %@\n With url: %@", error, task.response.URL);
-                                                    }];
+    static int kNumberOfImages = 20;
+    self.imageURLs = [NSMutableArray arrayWithCapacity:kNumberOfImages];
+    // A set of uploaded images on my public dropbox folder
+    for (int i = 0; i <= kNumberOfImages; i++) {
+        self.imageURLs[i] = [NSString stringWithFormat:@"%@%d.jpg", kStaticDropBoxURL, i];
+    }
+    
+    [self setDefaultRowHeights];
+    [self.tableView reloadData];
+    
 }
 
 #pragma mark - Table view data source
@@ -62,21 +56,27 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.flickrData.photos.count;
+    return self.imageURLs.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     DynamicTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"DynamicCell" forIndexPath:indexPath];
-    Photo *photo = self.flickrData.photos[indexPath.row];
+    cell.mainImageView.image = nil;
+    NSString *imageURL = self.imageURLs[indexPath.row];
     __weak DynamicTableViewCell *weakCell = cell;
     __weak typeof(self) weakSelf = self;
-    [cell.mainImageView setImageWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:photo.url]]
+    [cell.mainImageView setImageWithURLRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:imageURL]]
                               placeholderImage:nil
                                        success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
                                            weakCell.mainImageView.image = image;
                                            // Update table row heights
                                            NSInteger oldHeight = [weakSelf.tableRowHeights[indexPath.row] integerValue];
                                            NSInteger newHeight = (int)image.size.height;
+                                           
+                                           if (image.size.width > CGRectGetWidth(weakCell.mainImageView.bounds)) {
+                                               CGFloat ratio = image.size.height / image.size.width;
+                                               newHeight = CGRectGetWidth(self.view.bounds) * ratio;
+                                           }
                                            
                                            // Update table row height if image is in different size
                                            if (oldHeight != newHeight) {
@@ -86,7 +86,7 @@
                                                [weakSelf.tableView beginUpdates];
                                                
                                                // No need to call reloadRowsAtIndexPaths
-                                               // [weakSelf.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+                                               [weakSelf.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
                                                [weakSelf.tableView endUpdates];
                                            }
                                        } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
@@ -103,8 +103,8 @@
 #pragma mark - Updating row heights
 
 - (void)setDefaultRowHeights {
-    self.tableRowHeights = [NSMutableArray arrayWithCapacity:self.flickrData.photos.count];
-    for (int i = 0; i < self.flickrData.photos.count; i++) {
+    self.tableRowHeights = [NSMutableArray arrayWithCapacity:self.imageURLs.count];
+    for (int i = 0; i < self.imageURLs.count; i++) {
         self.tableRowHeights[i] = @(self.tableView.rowHeight);
     }
 }
