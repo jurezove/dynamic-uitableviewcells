080a518bd7a7da56e5d5ce27782214d0a0cdfe38
Jure Žove <jure.pase.kure@gmail.com>
2015-06-07 12:04:19 +0200

    removed files in gitignore

--- Pods/Target Support Files/Pods-Dynamic Table View Cells-AFNetworking/Pods-Dynamic Table View Cells-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dynamic_Table_View_Cells_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_Dynamic_Table_View_Cells_AFNetworking
-@end
